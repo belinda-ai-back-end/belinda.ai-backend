0f61857ddc54f7afc168c1a2eceecb9a50c41436
matveysofie <matveeva.sonya1703@gmail.com>
2023-08-31 23:02:02 +0300

    :loud_sound: for database :loud_sound:

--- belinda_app/schemas/__init__.py
+++ belinda_app/schemas/__init__.py
@@ -3,5 +3,3 @@ from .deal import CreateDealRequest
 from .musician import CreateMusicianRequest, MusicianLogin
 from .curator import CreateCuratorRequest, CuratorLogin
 from .musician_track import CreateMusicianTrackRequest
-from .musician_session import MusicianSessionSchema
-from .curator_session import CuratorSessionSchema
