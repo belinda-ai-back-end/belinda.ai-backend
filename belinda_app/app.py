4226de8fd48d52278584d6faa1162580564b5495
matveysofie <matveeva.sonya1703@gmail.com>
2023-09-12 19:28:25 +0300

    :loud_sound: for database :loud_sound:

--- belinda_app/app.py
+++ belinda_app/app.py
@@ -7,7 +7,7 @@ from starlette.middleware.sessions import SessionMiddleware
 from belinda_app.routes import router
 from belinda_app.db.database import init_db
 from belinda_app.settings import get_settings
-from belinda_app.utils import setup_logger, track   # track
+from belinda_app.utils import setup_logger   # track
 
 logger = logging.getLogger(__name__)
 
@@ -26,7 +26,7 @@ async def on_startup():
     app.include_router(router)
     setup_logger()
     logger.error(settings)
-    await track()
+    # await track()
 
 
 app.add_middleware(SessionMiddleware, secret_key=settings.SESSION_SECRET_KEY)
