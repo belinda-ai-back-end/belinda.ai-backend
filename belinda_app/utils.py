22c50f6c2d0c9583511a2dae4b8d57e6b49f32a5
matveysofie <matveeva.sonya1703@gmail.com>
2023-09-17 19:37:42 +0300

    :loud_sound: for database :loud_sound:

--- belinda_app/utils.py
+++ belinda_app/utils.py
@@ -1,11 +1,11 @@
-# import json
+import json
 import logging
 
 from fastapi.logger import logger
 from fastapi import Request
 from jwt import decode
-# from belinda_app.models import Track
-# from belinda_app.db.database import SessionLocal
+from belinda_app.models import Track
+from belinda_app.db.database import SessionLocal
 from belinda_app.settings import get_settings
 
 settings = get_settings()
@@ -25,62 +25,63 @@ def get_user_id(request: Request):
     token_data = decode(jwt=access_token, key=SECRET_KEY, algorithms=["HS256"])
     return token_data["user_id"]
 
+
 # Скрипт валидации json
-# async def track():
-#     with open('tracks.json', 'r') as file:
-#         content = file.read()
-#     async with SessionLocal() as session:
-#         json_objects = content.split('\n')
-#         fixed_json_objects = []
-#
-#         for json_str in json_objects:
-#             try:
-#                 json_obj = json.loads(json_str)
-#                 fixed_json_objects.append(json_obj)
-#             except json.JSONDecodeError:
-#                 pass
-#
-#         fixed_result = '['
-#
-#         for i, json_obj in enumerate(fixed_json_objects):
-#             fixed_result += json.dumps(json_obj)
-#
-#             if i < len(fixed_json_objects) - 1:
-#                 fixed_result += ','
-#
-#         fixed_result += ']'
-#         track_data_list = json.loads(fixed_result)
-#
-#         for track_data in track_data_list:
-#             albumTotalTracks = track_data["album"].get(
-#                 "total_tracks")
-#
-#             artists = track_data["album"].get("artists", [])
-#             if artists:
-#                 artistId = artists[0].get("id")
-#                 artistName = artists[0].get("name")
-#                 artistHref = artists[0].get("href")
-#             else:
-#                 artistId = None
-#                 artistName = None
-#                 artistHref = None
-#
-#             track = Track(
-#                 id=track_data.get("id"),
-#                 durationMs=track_data.get("duration_ms"),
-#                 name=track_data.get("name"),
-#                 popularity=track_data.get("popularity"),
-#                 previewUrl=track_data.get("preview_url"),
-#                 albumId=track_data["album"].get("id"),
-#                 albumHref=track_data["album"].get("href"),
-#                 albumName=track_data["album"].get("name"),
-#                 albumTotalTracks=albumTotalTracks,
-#                 artistId=artistId,
-#                 artistName=artistName,
-#                 artistHref=artistHref,
-#                 playlist_id=track_data.get("playlists", [None])[0],
-#             )
-#             session.add(track)
-#
-#         await session.commit()
-#         return {"message": "Data uploaded successfully"}
+async def track():
+    with open('tracks.json', 'r') as file:
+        content = file.read()
+    async with SessionLocal() as session:
+        json_objects = content.split('\n')
+        fixed_json_objects = []
+
+        for json_str in json_objects:
+            try:
+                json_obj = json.loads(json_str)
+                fixed_json_objects.append(json_obj)
+            except json.JSONDecodeError:
+                pass
+
+        fixed_result = '['
+
+        for i, json_obj in enumerate(fixed_json_objects):
+            fixed_result += json.dumps(json_obj)
+
+            if i < len(fixed_json_objects) - 1:
+                fixed_result += ','
+
+        fixed_result += ']'
+        track_data_list = json.loads(fixed_result)
+
+        for track_data in track_data_list:
+            albumTotalTracks = track_data["album"].get(
+                "total_tracks")
+
+            artists = track_data["album"].get("artists", [])
+            if artists:
+                artistId = artists[0].get("id")
+                artistName = artists[0].get("name")
+                artistHref = artists[0].get("href")
+            else:
+                artistId = None
+                artistName = None
+                artistHref = None
+
+            track = Track(
+                id=track_data.get("id"),
+                durationMs=track_data.get("duration_ms"),
+                name=track_data.get("name"),
+                popularity=track_data.get("popularity"),
+                previewUrl=track_data.get("preview_url"),
+                albumId=track_data["album"].get("id"),
+                albumHref=track_data["album"].get("href"),
+                albumName=track_data["album"].get("name"),
+                albumTotalTracks=albumTotalTracks,
+                artistId=artistId,
+                artistName=artistName,
+                artistHref=artistHref,
+                playlist_id=track_data.get("playlists", [None])[0],
+            )
+            session.add(track)
+
+        await session.commit()
+        return {"message": "Data uploaded successfully"}
